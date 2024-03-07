8d609e6f0dbba659c9ddcb5daab6905c35ff366a
flavia1luiza2 <160907162+flavia1luiza2@users.noreply.github.com>
2024-03-07 15:07:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const botoes = document.querySelectorAll("botao");
+
+for(let 1=0;i <length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classlist.remove("ativo");
+    }
+    console.log();
+}
